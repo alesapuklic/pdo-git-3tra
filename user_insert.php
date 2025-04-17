9bc6a314609dec28de2843814ce5a2df659b41f9
alesapuklic <146018307+alesapuklic@users.noreply.github.com>
2025-04-17 12:19:54 +0200

    dodan user insert

---
+++ user_insert.php
@@ -0,0 +1,13 @@
+<?php
+
+include("database_.php");
+
+$email = $_POST["email"];
+$pass = $_POST["pass"];
+
+$sql = "INSERT INTO users (email, pass) VALUES (?, ?)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$email, $pass]);
+
+header("Location: users.php");
+
